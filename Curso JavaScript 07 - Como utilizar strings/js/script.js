7026f7021f538851858006c95edf1de70b7290c9
Eduardo <103686965+DevEduardoSouza@users.noreply.github.com>
2023-06-29 08:44:14 -0300

    Adicionando as anotações da aula 07

--- Curso JavaScript 07 - Como utilizar strings/js/script.js
+++ Curso JavaScript 07 - Como utilizar strings/js/script.js
@@ -1,1 +1,18 @@
-// Adicione o código JavaScript aqui
+// Adicione o cï¿½digo JavaScript aqui
+
+/** String
+ *  
+ *  - tipo de dado para texto
+ *  - formas = '' ou ""
+ *  - concatenar usa o +
+ * 
+ */
+
+var nome = "Eduardo";
+var sobreNome = " Souza"
+
+var nomeComplto = nome + sobreNome;
+
+console.log(nome);
+console.log(typeof nome);
+console.log(nomeComplto);
