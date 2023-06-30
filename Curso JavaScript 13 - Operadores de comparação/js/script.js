417be715ac319f156bb576b819a15d865611ede2
Eduardo <103686965+DevEduardoSouza@users.noreply.github.com>
2023-06-30 08:33:47 -0300

    Add anotações da aula 13

--- Curso JavaScript 13 - Operadores de comparação/js/script.js
+++ Curso JavaScript 13 - Operadores de comparação/js/script.js
@@ -1,1 +1,28 @@
-// Adicione o c�digo JavaScript aqui
+// Adicione o c�digo JavaScript aqui
+
+/** Operadores de comparação
+ * 
+ *  - == , != , >= , <= , < , >
+ *  - obter true ou false
+ * 
+ */
+
+var idade = 18;
+
+if(idade>=18){
+  console.log("Pode fazer a carteira");
+}
+
+if(idade<=17){
+  console.log("NÃo pode fazer a carteira");
+}
+
+var nome = "Eduardo";
+
+if(nome == "Eduardo"){
+  console.log("O seu nome é Eduardo");
+}
+
+if(nome != "Souza"){
+  console.log("O seu nome não é Souza");
+}
