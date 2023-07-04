8d9ae469977f26e28496ba1c35b73e54cc14ab10
Eduardo <103686965+DevEduardoSouza@users.noreply.github.com>
2023-07-04 09:49:19 -0300

    Add anotações da aula 21

--- Curso JavaScript 21 - for (estrutura de repetição/js/script.js
+++ Curso JavaScript 21 - for (estrutura de repetição/js/script.js
@@ -1,1 +1,16 @@
-// Adicione o código JavaScript aqui
+// Adicione o cï¿½digo JavaScript aqui
+
+/**   FOR
+ * 
+ */
+
+for (let i = 0; i< 10; i++){
+  console.log("For " + 1 );
+}
+
+
+var arr = [1,2,3,4];
+
+for(let i = 0 ; i < arr.length; i++){
+  console.log(arr[i]);
+}
