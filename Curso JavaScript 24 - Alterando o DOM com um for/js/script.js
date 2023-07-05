ba3fdc6dbfe802c5988b7914a27a7e905ba4e250
Eduardo <103686965+DevEduardoSouza@users.noreply.github.com>
2023-07-05 08:46:18 -0300

    Add anotações da aula 24

--- Curso JavaScript 24 - Alterando o DOM com um for/js/script.js
+++ Curso JavaScript 24 - Alterando o DOM com um for/js/script.js
@@ -1,1 +1,26 @@
-// Adicione o código JavaScript aqui
+// Adicione o cï¿½digo JavaScript aqui
+
+/** Alterando DOM com FOR
+ */
+
+var lista = ['laranja', 'maÃ§a', 'uva'];
+
+var ul = document.createElement('ul');
+
+var body = document.getElementsByTagName('body');
+
+body[0].appendChild(ul);
+
+var listaUl = document.getElementsByTagName('ul');
+
+
+for (let index = 0; index < lista.length; index++) {
+   var li = document.createElement('li');
+
+   var texto = document.createTextNode(lista[index]);
+
+   li.appendChild(texto);
+
+   listaUl[0].appendChild(li);
+  
+}
