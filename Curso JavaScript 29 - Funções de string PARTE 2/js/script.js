fa3c1a45de33fdc93f3c0d38afde1aef2fa90f7e
Eduardo <103686965+DevEduardoSouza@users.noreply.github.com>
2023-07-06 15:14:23 -0300

    Add anotações da aula 29

--- Curso JavaScript 29 - Funções de string PARTE 2/js/script.js
+++ Curso JavaScript 29 - Funções de string PARTE 2/js/script.js
@@ -1,1 +1,30 @@
-// Adicione o c�digo JavaScript aqui
+// Adicione o c�digo JavaScript aqui
+
+/** Funções de string  2
+ * - usado para modificar
+ * 
+ */
+
+
+
+
+var nome = 'Eduardo';
+
+
+//toLowCase -> transformar tudo em caixa baixa
+console.log(nome.toLowerCase());
+
+//toUpperCase -> transformar tudo em caixa alta
+console.log(nome.toUpperCase());
+
+// trim -> remover espaços da string
+var nome = "     Eduardo de Souza    ";
+
+console.log(nome.trim());
+
+// split -> dividir e trasnformar cada palavra e add em um array
+
+var tags = "Html, js, react, java, css";
+
+console.log(tags.split(","));
+
