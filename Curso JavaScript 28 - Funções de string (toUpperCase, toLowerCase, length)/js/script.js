13501b6ab89fdd3f71228533c7d83551d1339a47
Eduardo <103686965+DevEduardoSouza@users.noreply.github.com>
2023-07-06 14:55:08 -0300

    Add anotações da alua 28

--- Curso JavaScript 28 - Funções de string (toUpperCase, toLowerCase, length)/js/script.js
+++ Curso JavaScript 28 - Funções de string (toUpperCase, toLowerCase, length)/js/script.js
@@ -1,1 +1,31 @@
-// Adicione o c�digo JavaScript aqui
+// Adicione o c�digo JavaScript aqui
+
+/** MÉTODOS DE STRING
+ *  - O objeto String possui métodos úteis
+ * 
+ */
+
+// length -> vai retornar a quantidade de elementos/caracteres da string
+
+var nome = "Eduardo";
+
+console.log(nome.length);
+
+// indexOf -> vai dar o index do caractere/palavra que eu queira encontrar
+// se não achar ele retorna -1
+console.log(nome.indexOf("Eduardo"));
+
+var frase = "Estou aprendendo JavaScript";
+console.log(frase.indexOf("JavaScript"));
+
+// Slice via remover uma parte da string, ele recebe o incio e fim com parâmetro
+
+var js = frase.slice(17, 27);
+
+console.log(js);
+
+// replace posso trocar um palavra da String, ele recebe a string a ser trocada logo depois string a ser adicionada 
+
+var novaFrase = frase.replace("Estou", "estou,");
+
+console.log(novaFrase);
