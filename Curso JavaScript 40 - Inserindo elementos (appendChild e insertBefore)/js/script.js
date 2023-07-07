e16fe99edf40003550beabbb53c442a419bada36
Eduardo <103686965+DevEduardoSouza@users.noreply.github.com>
2023-07-07 15:30:54 -0300

    Add anotações da aula 40

--- Curso JavaScript 40 - Inserindo elementos (appendChild e insertBefore)/js/script.js
+++ Curso JavaScript 40 - Inserindo elementos (appendChild e insertBefore)/js/script.js
@@ -1,1 +1,22 @@
-// Adicione o código JavaScript aqui
+// Adicione o cï¿½digo JavaScript aqui
+
+/** Inserindo elementos (appendChild)
+ * 
+ *  - Podemos inserir um elemento dentro de outro
+ * 
+ */
+
+// Criar elemento
+
+var el = document.createElement('div');
+
+// Adicionar uma classe ao elemento
+el.classList = "div-teste";
+console.log(el);
+
+
+var container = document.querySelector(".container");
+container.appendChild(el);
+
+
+
