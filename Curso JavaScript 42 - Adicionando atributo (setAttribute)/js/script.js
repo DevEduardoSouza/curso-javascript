afa6aba970098f135a7013528c990532f0e5b43c
Eduardo <103686965+DevEduardoSouza@users.noreply.github.com>
2023-07-08 10:34:24 -0300

    Add anotações da aula 42

--- Curso JavaScript 42 - Adicionando atributo (setAttribute)/js/script.js
+++ Curso JavaScript 42 - Adicionando atributo (setAttribute)/js/script.js
@@ -1,1 +1,20 @@
-// Adicione o código JavaScript aqui
+// Adicione o cï¿½digo JavaScript aqui
+
+/** Adicionando atributo (setAttribute)
+ * 
+ * - posso mudar , alt , src , id , classe e outros.
+ * - pode alterar qualquer atributo via JS
+ */
+
+var subtitulo = document.querySelector('.title');
+
+subtitulo.setAttribute('class' , 'novaClasse');
+console.log(subtitulo);
+
+var btn = document.querySelector('.btn');
+
+btn.setAttribute('disabled', 'disabled');
+
+// Remover atributo
+
+subtitulo.removeAttribute('novaClasse');
