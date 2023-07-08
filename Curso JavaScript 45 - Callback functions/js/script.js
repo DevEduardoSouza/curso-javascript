40f0a6e46aafe591711f08a81e2a6d61a330afcc
Eduardo <103686965+DevEduardoSouza@users.noreply.github.com>
2023-07-08 14:59:37 -0300

    Add anotações da aula 45

--- Curso JavaScript 45 - Callback functions/js/script.js
+++ Curso JavaScript 45 - Callback functions/js/script.js
@@ -1,1 +1,27 @@
-// Adicione o c�digo JavaScript aqui
+// Adicione o c�digo JavaScript aqui
+
+/** Callback functions
+ * 
+ *  -Permite executar uma função depois  de uma detarminada ação
+ *  -Fundametal para entender a parte de  assíncrona  do JS
+ * 
+ */
+
+
+
+function exibir(num) {
+  console.log(num);
+}
+
+
+function soma(a, b, callback) {
+  var op = a + b;
+  callback(op);
+}
+function multiplicacao(a, b, cb) {
+  var op = a * b;
+  cb(op);
+}
+
+soma(5 , 5, exibir);
+multiplicacao(3,2, exibir);
