6c0cf893c70bf6b4e1d0935f5f8435bb4e7fecfa
Eduardo <103686965+DevEduardoSouza@users.noreply.github.com>
2023-07-08 10:09:01 -0300

    Add anotações da aula 41

--- Curso JavaScript 41 - Trocando elementos (replaceChild)/js/script.js
+++ Curso JavaScript 41 - Trocando elementos (replaceChild)/js/script.js
@@ -1,1 +1,37 @@
-// Adicione o c�digo JavaScript aqui
+// Adicione o c�digo JavaScript aqui
+
+/** Trocando elementos (replaceChild)
+ * 
+ * - Posso substituir uma tag por outra de modo fácil
+ */
+
+
+//  1° Criar o elemento
+var el = document.createElement('div');
+
+el.classList = 'conateiner2';
+
+var texto = document.createTextNode('Texto da div');
+
+el.appendChild(texto);
+
+console.log(el);
+
+// Agora selecionar o elemento que eu quero trocar
+var div = document.querySelector('.container');
+
+
+// Selecionar o pai do elemento que quero trocar
+var body = document.querySelector('body');
+// OU
+var body = div.parentNode;
+
+
+/** trocar os elementos
+ *  1° pai.replaceChild
+ *  2° pai.replaceChild(elemento que vai entrar, )
+ *  3° pai.replaceChild(elemento que vai entrar, elemento qu vai sair)
+ */
+
+body.replaceChild(el, div);
+
