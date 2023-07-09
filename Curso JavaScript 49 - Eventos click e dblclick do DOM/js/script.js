4e706ceb57aa006991848aff0b87010b97a9a8f8
Eduardo <103686965+DevEduardoSouza@users.noreply.github.com>
2023-07-09 14:54:55 -0300

    Add anotações da aula 49

--- Curso JavaScript 49 - Eventos click e dblclick do DOM/js/script.js
+++ Curso JavaScript 49 - Eventos click e dblclick do DOM/js/script.js
@@ -1,1 +1,25 @@
-// Adicione o código JavaScript aqui
+// Adicione o cï¿½digo JavaScript aqui
+
+/** Eventos click e dblclick do DOM
+ * 
+ * 
+ */
+
+var btn = document.querySelector('#btn');
+var btn2 = document.querySelector('#btn2');
+
+var title = document.querySelector('h1');
+
+
+// addEventListener
+btn.addEventListener('click', ()=>{
+
+  title.style.background = '#000';
+  
+  console.log('Clicou');
+});
+
+// Doule click
+btn2.addEventListener('dblclick', ()=>{
+  console.log('Double click');
+});
