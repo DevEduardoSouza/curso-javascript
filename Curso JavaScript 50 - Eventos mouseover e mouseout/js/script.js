13ad2a8f68aca154df3296dafdaa59f7c23132bb
Eduardo <103686965+DevEduardoSouza@users.noreply.github.com>
2023-07-09 15:04:55 -0300

    Add anotações da aula 50

--- Curso JavaScript 50 - Eventos mouseover e mouseout/js/script.js
+++ Curso JavaScript 50 - Eventos mouseover e mouseout/js/script.js
@@ -1,1 +1,15 @@
-// Adicione o código JavaScript aqui
+// Adicione o cï¿½digo JavaScript aqui
+
+/** Eventos mouseover e mouseout
+ * 
+ *  - Quando o mouse sai ou entra em um elemento
+ */
+
+var title = document.querySelector('h1');
+
+title.addEventListener('mouseover', ()=>{
+ console.log('Entrou com mouse');
+});
+title.addEventListener('mouseout', ()=>{
+ console.log('saiu com mouse');
+});
