f31cc5f7bc1766318f7fc78b20181bbb57c9aab3
Eduardo <103686965+DevEduardoSouza@users.noreply.github.com>
2023-07-09 15:14:11 -0300

    Add anotações da aula 51

--- Curso JavaScript 51 - Eventos keydown e keyup/js/script.js
+++ Curso JavaScript 51 - Eventos keydown e keyup/js/script.js
@@ -1,1 +1,22 @@
-// Adicione o código JavaScript aqui
+// Adicione o cï¿½digo JavaScript aqui
+
+
+/** Eventos keydown e keyup
+ * - Eventos acionados quanto Ã© pressionado um tecla
+ * 
+ */
+
+document.addEventListener('keydown', (event)=>{
+
+  if(event.key === 'Enter'){
+    console.log('Apertou o enter')
+  }
+});
+document.addEventListener('keyup', (event)=>{
+  
+
+    if(event.key === 'Enter'){
+      console.log('Soltou o enter')
+    }
+  
+});
