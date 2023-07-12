c98644d038ec8256ab843af34402fdfeac13d5fb
Eduardo <103686965+DevEduardoSouza@users.noreply.github.com>
2023-07-12 15:20:47 -0300

    Add anotações sobre Tipos de dados

--- Curso JavaScript 57 - aprenda os fundamentos de JavaScript/js/script.js
+++ Curso JavaScript 57 - aprenda os fundamentos de JavaScript/js/script.js
@@ -23,4 +23,37 @@ console.log(x);
 console.log(y);
 console.log(z);
 
-/* ---------------{}--------------- */
+/* ---------------{Tipos de dados}--------------- */
+
+const name = 'Eduardo'; //String
+console.log(name);
+console.log(typeof name); //typeof vai sempre informa o tipo de dado
+
+const shirtsQty = 4; //Númerico
+console.log(shirtsQty);
+console.log(typeof shirtsQty);
+
+const isApproved = false; //Bollean, true ou false
+console.log(isApproved);
+console.log(typeof isApproved);
+
+let surname = null; //Indica que essa variável ainda não tem valor
+console.log(surname);
+console.log(typeof surname);
+
+let age; // Undefined o dado não foi definido
+console.log(age);
+console.log(typeof age);
+
+const languages = ['C','Js','Java']; //Array
+console.log(languages);
+console.log(typeof languages);
+
+// object literals -> não veio aparti de uma classe
+const user = {
+  email:'eduardo@gmail.com',
+  age: 21
+};
+console.log(user);
+console.log(typeof user);
+
