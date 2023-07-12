01f72a7a136226b2a4baee91fc38a6d49d667e0e
Eduardo <103686965+DevEduardoSouza@users.noreply.github.com>
2023-07-12 16:38:12 -0300

    Add anotações sobre JSON

--- Curso JavaScript 57 - aprenda os fundamentos de JavaScript/js/script.js
+++ Curso JavaScript 57 - aprenda os fundamentos de JavaScript/js/script.js
@@ -122,3 +122,44 @@ console.log(product['Main color']);
 const {price, inStock} = product;
 console.log(price);
 console.log(inStock);
+
+
+/* ---------------{JSON}--------------- */
+
+const objs = [
+  {
+    nome : 'Eduardo',
+    idade : 21,
+    esta_estudando : true,
+    detalhes_curso : {
+      nome : 'IFBA',
+      curso : 'ADS',
+      total_semestre : 6
+    },
+    linguagens : ["JS", "Java", "C"],
+  },
+  {
+    nome : 'Teste',
+    idade : 25,
+    esta_estudando : false,
+    detalhes_curso : {
+      nome : null,
+      curso : null,
+      total_semestre : null
+    },
+    linguagens : ["TE", "Java", "C"],
+  },
+]
+
+// Como enviar para uma API
+// Converte o Objeto para JSON antes de enviar para uma API
+const jsonData =  JSON.stringify(objs);
+console.log(jsonData);
+
+// Coverte a String para json quando receber da API
+const objData = JSON.parse(jsonData);
+console.log(objData);
+
+objData.map((pessoa)=>{
+  console.log(pessoa.nome);
+});
