3b3d535b161a7dd97785a7e55473c288a9069ed3
Eduardo <103686965+DevEduardoSouza@users.noreply.github.com>
2023-07-12 14:52:33 -0300

    Add anotações sobre console

--- Curso JavaScript 57 - aprenda os fundamentos de JavaScript/js/script.js
+++ Curso JavaScript 57 - aprenda os fundamentos de JavaScript/js/script.js
@@ -1,3 +1,16 @@
 // Adicione o c�digo JavaScript aqui
 
 // Revisão e mais!
+
+/* ---------------{Console}--------------- */
+
+console.log('Depurando o código'); //Depurar o código
+console.error('Erro!');  //Mensagem de erro
+console.warn('Aviso!');  //Mensagem de Aviso
+
+
+
+
+/* ---------------{Console}--------------- */
+
+
