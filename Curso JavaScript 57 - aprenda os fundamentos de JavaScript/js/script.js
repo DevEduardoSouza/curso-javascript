d5747e09ef898f3cc340c0d1fd9ef9caf5e25bea
Eduardo <103686965+DevEduardoSouza@users.noreply.github.com>
2023-07-12 15:03:03 -0300

    Add anotações sobre Variáveis

--- Curso JavaScript 57 - aprenda os fundamentos de JavaScript/js/script.js
+++ Curso JavaScript 57 - aprenda os fundamentos de JavaScript/js/script.js
@@ -6,11 +6,21 @@
 
 console.log('Depurando o código'); //Depurar o código
 console.error('Erro!');  //Mensagem de erro
-console.warn('Aviso!');  //Mensagem de Aviso
+console.warn('Aviso!');  //Mensagem de Aviso 
 
 
+/* ---------------{Variáveis}--------------- */
 
+var x = 10; 
+// Mais mordenas
+let y = 20;   //Consigo mudar os valores, escopo de bloco
+const z = 30; //NÃO Consigo mudar os valores, escopo de bloco
 
-/* ---------------{Console}--------------- */
+if(true){
+  let y = 25;
+}
+console.log(x);
+console.log(y);
+console.log(z);
 
-
+/* ---------------{}--------------- */
