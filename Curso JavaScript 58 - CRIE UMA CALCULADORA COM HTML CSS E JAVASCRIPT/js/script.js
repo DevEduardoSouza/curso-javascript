8fd90e6801d65fc7a537476c5732b78e983e5949
Eduardo <103686965+DevEduardoSouza@users.noreply.github.com>
2023-07-13 19:15:41 -0300

    Criando o obj Calculator

--- Curso JavaScript 58 - CRIE UMA CALCULADORA COM HTML CSS E JAVASCRIPT/js/script.js
+++ Curso JavaScript 58 - CRIE UMA CALCULADORA COM HTML CSS E JAVASCRIPT/js/script.js
@@ -8,8 +8,14 @@ const currentOperationText = document.querySelector('#current-operation');
 const buttons = document.querySelectorAll('#buttons-container button');
 
 class Calculator{
-
+  constructor(previousOperationText, currentOperationText){
+    this.previousOperationText = previousOperationText;
+    this.currentOperationText = currentOperationText;
+    this.currentOperation = "";
+  }
 }
+
+const calc = new Calculator(previousOperationText, currentOperationText);
 
 buttons.forEach((button)=>{
   button.addEventListener('click', (e)=>{
@@ -20,7 +26,6 @@ buttons.forEach((button)=>{
     if(+value >= 0 || value === '.'){
       console.log(value);
     }else{
-      
       console.log("op" +value);
     }
 
