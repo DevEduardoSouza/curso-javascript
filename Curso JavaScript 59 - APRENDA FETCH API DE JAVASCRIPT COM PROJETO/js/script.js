e44f3065bb53d6c448b3b9bd0bb3d8940f2a5cca
Eduardo <103686965+DevEduardoSouza@users.noreply.github.com>
2023-07-14 22:05:18 -0300

    Add os dados da API na página

--- Curso JavaScript 59 - APRENDA FETCH API DE JAVASCRIPT COM PROJETO/js/script.js
+++ Curso JavaScript 59 - APRENDA FETCH API DE JAVASCRIPT COM PROJETO/js/script.js
@@ -12,8 +12,46 @@
 // Criar variáveis que serão usadas ao longo do código
 
 // Url da API
-const url = "https://jsonplaceholder.typicode.com/post";
+const url = "https://jsonplaceholder.typicode.com/posts";
 // Selcionar o loading
 const loading = document.querySelector('#loading');
 // Selecionar o post container
 const postsContainer = document.querySelector('#post-container');
+
+// Pegar todos os posts
+async function getAllPosts() {
+  const response = await fetch(url);
+  const data = await response.json();
+
+  // Esconder o elemento que mostra que está carregando
+  loading.classList.add('hide');
+
+  // Agora add no dom
+  data.map((post)=>{
+
+    // criar os elementos para ser renderizado na tela
+    const div = document.createElement('div');
+    const title = document.createElement('h2');
+    const body = document.createElement('p');
+    const link = document.createElement('a');
+
+    // Com elementos criados agora tenho que preencher com conteúdo
+    title.innerText = post.title;
+    body.innerText = post.body;
+    link.innerText = "Ler";
+    link.setAttribute('href', `/post.html?id=${post.id}`);
+    
+
+    // Add os elementos na div
+    div.appendChild(title);
+    div.appendChild(body);
+    div.appendChild(link);
+   
+    // Add a div no postsContainer
+    postsContainer.appendChild(div);
+   
+  }); 
+
+}
+
+getAllPosts();
