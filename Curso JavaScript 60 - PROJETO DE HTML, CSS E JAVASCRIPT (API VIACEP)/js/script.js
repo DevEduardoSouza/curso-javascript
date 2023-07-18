d5238faa69d03e9b905461255fa93fadead2df66
Eduardo <103686965+DevEduardoSouza@users.noreply.github.com>
2023-07-18 08:44:30 -0300

    Selecionando os elementos que vão ser usados

--- Curso JavaScript 60 - PROJETO DE HTML, CSS E JAVASCRIPT (API VIACEP)/js/script.js
+++ Curso JavaScript 60 - PROJETO DE HTML, CSS E JAVASCRIPT (API VIACEP)/js/script.js
@@ -1,1 +1,13 @@
-// Adicione o código JavaScript aqui
+// Adicione o cï¿½digo JavaScript aqui
+
+// Selecionar os elementos
+const addressForm = document.querySelector('#address-form');
+const cepInput = document.querySelector('#cep');
+const addressInput = document.querySelector('#address');
+const cityInput = document.querySelector('#city');
+const neigthborhoodInput = document.querySelector('#neigthborhood');
+const regionInput = document.querySelector('#region');
+// Inputs que vai ser manipulados em massa
+const formsInputs = document.querySelectorAll('[data-input]');
+
+const closeButton = document.querySelector('#close-message');
