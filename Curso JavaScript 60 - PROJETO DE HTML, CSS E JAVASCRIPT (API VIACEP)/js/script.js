352f5670eb96b70f2e686062543d3f30daa83be6
Eduardo <103686965+DevEduardoSouza@users.noreply.github.com>
2023-07-18 09:23:04 -0300

    Método para consultar a API

--- Curso JavaScript 60 - PROJETO DE HTML, CSS E JAVASCRIPT (API VIACEP)/js/script.js
+++ Curso JavaScript 60 - PROJETO DE HTML, CSS E JAVASCRIPT (API VIACEP)/js/script.js
@@ -25,3 +25,47 @@ cepInput.addEventListener('keypress', (e)=>{
   }
 
 });
+
+// Pegar o endereço quanto eu tiver os 8 dígitos no CEP
+cepInput.addEventListener('keyup', (e)=>{
+
+  const inputValue = e.target.value;
+
+  // Verificar se temos a quantidade de dígitos necessária
+  if(inputValue.length === 8){
+    getAddress(inputValue);
+  }
+});
+
+// Buscar na API
+const getAddress = async (cep) => {
+  toggleLoader();
+  console.log(cep);
+  
+  // tirar o cursor do cep enquanto está carregando
+  cepInput.blur();
+  
+  const apiUrl = `https://viacep.com.br/ws/${cep}/json/`
+  const response = await fetch(apiUrl);
+  const data = await response.json();
+  
+  
+  // Mostra erro e resetar form
+  if(data.erro){
+    // Método nativo que vai limpar o formulário
+    addressForm.reset();
+    toggleLoader();
+    // Mostra a message
+    return;
+  }
+
+}
+
+// Mostra ou esconder loading
+const toggleLoader = () =>{
+  const fadeElement = document.querySelector("#fade");
+  const loaderElement = document.querySelector("#loader");
+
+  fadeElement.classList.toggle('hide');
+  loaderElement.classList.toggle('hide');
+}
