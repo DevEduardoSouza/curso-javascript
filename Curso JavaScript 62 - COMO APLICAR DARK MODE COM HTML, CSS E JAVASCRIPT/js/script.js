3b5a7f6fb975441023162b1d1bc081343cbefe11
Eduardo <103686965+DevEduardoSouza@users.noreply.github.com>
2023-07-19 09:00:39 -0300

    Finalizando o projeto

--- Curso JavaScript 62 - COMO APLICAR DARK MODE COM HTML, CSS E JAVASCRIPT/js/script.js
+++ Curso JavaScript 62 - COMO APLICAR DARK MODE COM HTML, CSS E JAVASCRIPT/js/script.js
@@ -1,5 +1,29 @@
 // Adicione o c�digo JavaScript aqui
+
+// Função dark mode
+function toggleDarkModel() {
+  document.body.classList.toggle('dark');
+}
+
+// Função load light ou dark
+function loadTheme() {
+  const darkMode = localStorage.getItem('dark');
+  if(darkMode){
+    toggleDarkModel();
+  }
+  
+}
+loadTheme();
+
 const changeThemeBtn = document.querySelector('#change-theme');
 changeThemeBtn.addEventListener('change', ()=>{
-    document.body.classList.toggle('dark');
+  toggleDarkModel();
+
+  // Salvar ou remocer dark mode
+  localStorage.removeItem('dark');
+
+
+  if(document.body.classList.contains('dark')){
+    localStorage.setItem('dark', 1);
+  }
 });
