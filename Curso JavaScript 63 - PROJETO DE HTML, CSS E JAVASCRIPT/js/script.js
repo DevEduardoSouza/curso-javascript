fc0319a84191ab861227871cbd5cd14857d32640
Eduardo <103686965+DevEduardoSouza@users.noreply.github.com>
2023-07-19 18:27:24 -0300

    Finalizando o projeto

--- Curso JavaScript 63 - PROJETO DE HTML, CSS E JAVASCRIPT/js/script.js
+++ Curso JavaScript 63 - PROJETO DE HTML, CSS E JAVASCRIPT/js/script.js
@@ -1,1 +1,14 @@
-// Adicione o código JavaScript aqui
+// Adicione o cï¿½digo JavaScript aqui
+
+const openModalButton = document.querySelector('#open-modal');
+const closeModalButton = document.querySelector('#close-modal');
+const modal = document.querySelector('#modal');
+const fade = document.querySelector('#fade'); 
+
+const toggleModal = () =>{
+  [modal, fade].forEach((el)=> el.classList.toggle('hide'));
+}
+// Add envento de uma forma mais prÃ¡tica
+[openModalButton, closeModalButton, fade].forEach((el)=>{
+  el.addEventListener('click', () => toggleModal());
+})
